81468356739ae960b67e2ba358a61de1dc898cbb
Lorenzo Fiorini <lorenfiorini@live.com>
2023-05-30 02:13:26 +0200

    philosopher struct

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 01:54:22 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/05/30 01:57:37 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/05/30 02:12:34 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,29 @@
 # define PHILO_H
 
 # include <pthread.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_philo
+{
+	pthread_t		thread;
+	long			id;
+	long			meals;
+	long			fork[2];
+	long			last_meal;
+	pthread_mutex_t	meal_time_lock;
+	struct s_table	*table;
+}	t_philo;
+
+typedef struct s_table
+{
+	long	num_philos;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	must_eat_count;
+	long	start_time;
+}	t_table;
 
 
 #endif
--- philo/source/1_main.c
+++ philo/source/1_main.c
@@ -6,7 +6,7 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 01:55:09 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/05/30 01:57:34 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/05/30 02:00:13 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,18 @@
 
 int	main(int argc, char **argv)
 {
-	
+	t_philo	*philo;
+
+	if (argc < 5 || argc > 6)
+		return (1);
+	philo = init_philo(argc, argv);
+	if (!philo)
+		return (1);
+	if (philo->error)
+	{
+		free(philo);
+		return (1);
+	}
 	
 	return (0);
 }
