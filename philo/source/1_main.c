e2645db78353b7d6237ebd4f7c03802ef8c59605
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-07-07 23:36:17 +0200

    table initialed properly To-do: segmentation fault on start()

--- philo/source/1_main.c
+++ philo/source/1_main.c
@@ -6,7 +6,7 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 01:55:09 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/07/07 23:29:20 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/07/07 23:36:10 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ int	main(int argc, char **argv)
 	if (!table)
 		return (error_msg(table, "Error: Malloc failed\n", 1));
 	parse_args(argc, argv, table);
-	printf("<- Parsed args\n");	// DEBUG
+	printf("<- Parsed args\n");		// DEBUG
 	if (!init(table))
 		return (error_msg(table, "Error: Malloc failed\n", 1));
-	printf("<- Initialized\n");	// DEBUG
+	printf("<- Initialized\n");		// DEBUG
 	show_table(table, 1); 			// DEBUG
 	if (!start(table))
 		return (1);
-	printf("<- Started\n");		// DEBUG
+	printf("<- Started\n");			// DEBUG
 	stop(table);
 	return (0);
 }
