53b74be9af46b3e8d730654000ff882214b1e67b
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-07-18 14:05:35 +0200

    main() & start_simulation()

--- philo_bonus/source/1_main_bonus.c
+++ philo_bonus/source/1_main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 01:55:09 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/07/17 20:41:55 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/07/18 14:04:33 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,39 @@ int	main(int argc, char **argv)
 		return (error_msg(NULL, STR_USAGE, 1));
 	table = malloc(sizeof(t_table));
 	if (!table)
-		return (error_msg(table, "Error: Malloc failed\n", 1));
+		return (error_msg(table, "Error: Malloc failed.", 1));
+	parse_args(argc, argv, table);
+	if (!init(table))
+		return (error_msg(table, "Error: Malloc failed.", 1));
+	if (!start_simulation(table))
+		return (1);
+	if (stop_simulation(table) == -1)
+		return (table_cleanup(table, 1));
+	return (table_cleanup(table, 0));
+}
 
-	return (0);
+int	start_simulation(t_table *table)
+{
+	int		i;
+	pid_t	pid;
+
+	table->start_time = get_time_in_ms() + (table->num_philos * 20);
+	i = 0;
+	while (i < table->num_philos)
+	{
+		pid = fork();
+		if (pid == -1)
+			return (error_msg(table, "Error: Could not fork child.", 1));
+		else if (pid > 0)
+			table->pids[i] = pid;
+		else if (pid == 0)
+		{
+			table->this_philo = table->philos[i];
+			philosopher(table);
+		}
+		i++;
+	}
+	if (!start_grim_reaper_threads(table))
+		return (0);
+	return (1);
 }
