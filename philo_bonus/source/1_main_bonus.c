5b52f8f18b48844ba0f67476ad4a82be003bc260
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-07-18 18:16:26 +0200

    start_grim_reaper_threads

--- philo_bonus/source/1_main_bonus.c
+++ philo_bonus/source/1_main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 01:55:09 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/07/18 14:04:33 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/07/18 16:26:53 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,26 @@ int	main(int argc, char **argv)
 		return (error_msg(NULL, STR_USAGE, 1));
 	table = malloc(sizeof(t_table));
 	if (!table)
-		return (error_msg(table, "Error: Malloc failed.", 1));
+		return (error_msg(table, MALLOC_ERROR, 1));
 	parse_args(argc, argv, table);
 	if (!init(table))
-		return (error_msg(table, "Error: Malloc failed.", 1));
+		return (error_msg(table, MALLOC_ERROR, 1));
 	if (!start_simulation(table))
 		return (1);
 	if (stop_simulation(table) == -1)
 		return (table_cleanup(table, 1));
 	return (table_cleanup(table, 0));
+}
+
+static int	start_grim_reaper_threads(t_table *table)
+{
+	if (pthread_create(&table->gluttony_reaper, NULL,
+			&global_gluttony_reaper, table) != 0)
+		return (error_msg(table, THREAD_ERROR, 0));
+	if (pthread_create(&table->famine_reaper, NULL,
+			&global_famine_reaper, table) != 0)
+		return (error_msg(table, THREAD_ERROR, 0));
+	return (1);
 }
 
 int	start_simulation(t_table *table)
@@ -42,7 +53,7 @@ int	start_simulation(t_table *table)
 	{
 		pid = fork();
 		if (pid == -1)
-			return (error_msg(table, "Error: Could not fork child.", 1));
+			return (error_msg(table, FORK_ERROR, 1));
 		else if (pid > 0)
 			table->pids[i] = pid;
 		else if (pid == 0)
